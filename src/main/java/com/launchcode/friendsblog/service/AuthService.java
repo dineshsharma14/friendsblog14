262bd9979f92f65b27483e37061aa60dacab3d24
dsharma <dinesh.sw3@gmail.com>
2021-01-19 11:11:00 -0600

    Added password encryption using PasswordEncoder interface

--- src/main/java/com/launchcode/friendsblog/service/AuthService.java
+++ src/main/java/com/launchcode/friendsblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.launchcode.friendsblog.dto.RegisterRequest;
 import com.launchcode.friendsblog.model.User;
 import com.launchcode.friendsblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +12,19 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 
 }
